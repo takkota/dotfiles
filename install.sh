dee90e6bef4ccd1265fe62cda408ca37fbda51d8
高橋宏太 <kota@kota-2.local>
2019-03-17 01:47:49 +0900

    added

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sh ./initialize.sh
 sh ./deploy.sh
 sh ./compose.sh
+echo 'if vim(neovim) does not work, try "pip3 uninstall neovim & pip3 install pynvim & pip3 install neovim"'
 echo 'set alfred synced with Dropbox manually.'
 echo 'load iterm preferences from dotfiles/settings.'
 echo 'sign in 1password'

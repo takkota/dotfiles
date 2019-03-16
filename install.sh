56656101cfdbfaadb8490c3d2213089464cb17c7
高橋宏太 <kota@kota-2.local>
2019-03-17 01:23:53 +0900

    追加

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 sh ./initialize.sh
 sh ./deploy.sh
 sh ./compose.sh
-echo 'set alfred synced with iCloud manually.'
+echo 'set alfred synced with Dropbox manually.'
 echo 'load iterm preferences from dotfiles/settings.'
+echo 'sign in 1password'
+echo 'set chrome user account to kotayuka0313@gmail.com'

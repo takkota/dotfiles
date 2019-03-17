eae9bd3d1c7bb157cea61b04398ccdee2b62a689
高橋宏太 <kota@kota-2.local>
2019-03-17 09:50:59 +0900

    added comment

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 sh ./initialize.sh
 sh ./deploy.sh
 sh ./compose.sh
-echo 'if vim(neovim) does not work, try "pip3 uninstall neovim & pip3 install pynvim & pip3 install neovim"'
+echo 'try launch vim(neovim). if vim(neovim) does not work, try "pip3 uninstall neovim & pip3 install pynvim & pip3 install neovim"'
 echo 'set alfred synced with Dropbox manually.'
 echo 'load iterm preferences from dotfiles/settings.'
 echo 'sign in 1password'
 echo 'set chrome user account to kotayuka0313@gmail.com'
+echo 'sign in to installed app manually.'
